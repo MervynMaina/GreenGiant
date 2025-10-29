f84d0c85d07eafb717548b97845bdf2fb3831211
princelit <mervynk020@gmail.com>
2025-10-29 08:37:40 +0300

    add

--- logisticsapp/models.py
+++ logisticsapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Quote(models.Model):
+    city_departure = models.CharField(max_length=100)
+    delivery_City = models.CharField(max_length=100)
+    total_Weight = models.IntegerField()
+    dimension = models.CharField(max_length=100)
+    shipment_Type = models.CharField(max_length=100)
+    urgency_Level = models.CharField(max_length=100)
+    fullname = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.IntegerField(max_length=100)
+    additional_info = models.TextField()
+
+    def __str__(self):
+        return self.fullname
--- logisticsapp/views.py
+++ logisticsapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from logisticsapp.models import *
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -11,8 +13,26 @@ def contact(request):
     return render(request, 'contact.html')
 
 def quote(request):
-    return render(request, 'get-a-quote.html')
+    if request.method == "POST":
+        myquote = Quote(
+            city_departure = request.POST['departure'],
+            delivery_city = request.POST['delivery'],
+            total_weight = request.POST['weight'],
+            dimension = request.POST['dimensions'],
+            shipment_type = request.POST['shipment_type'],
+            urgency_level = request.POST['urgency'],
+            fullname = request.POST['name'],
+            email = request.POST['email'],
+            phone = request.POST['phone'],
+            additional_info = request.POST['message'],
+        )
+        myquote.save()
+        messages.success(request, 'Your quotation is being processed!')
+        return redirect('/quote')
 
+    else:
+        messages.error(request, 'Unable to get a quote!.')
+        return render(request, 'get-a-quote.html')
 def pricing(request):
     return render(request, 'pricing.html')
 
